0168b254093e5c2b2e61ea8434c55c706bcbdf3e
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-06-08 17:25:35 +0900

    オブジェクトをNSCoding準拠でシリアライズして保存する処理を試行
    テストクラスに実装したので、試して直すのが速く回せて良い。

--- PlaySilializeObjectTests/PlaySilializeObjectTests.m
+++ PlaySilializeObjectTests/PlaySilializeObjectTests.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "Person.h"
+#import "Address.h"
 
 @interface PlaySilializeObjectTests : XCTestCase
 
@@ -45,6 +47,67 @@
     NSArray* after = [NSKeyedUnarchiver unarchiveObjectWithFile:filePath];
     
     XCTAssertEqualObjects(before, after);
+}
+
+-(void)testArchiveAndUnarchiveThreePeople{
+    XCTAssertTrue([self archivePeopleInfo]);
+    NSArray<Person*>* personsArray = [self personsArray];
+    NSArray<Person*> *loadedArray = [NSKeyedUnarchiver unarchiveObjectWithFile:[self filePath]];
+    
+    if (loadedArray) {
+        [loadedArray enumerateObjectsUsingBlock:^(Person* person, NSUInteger idx, BOOL *stop){
+            Person* referencedPerson = personsArray[idx];
+            XCTAssertEqualObjects(person.name, referencedPerson.name);
+            
+            Address* referencedAddress = referencedPerson.address;
+            XCTAssertEqualObjects(person.address.zipCode, referencedAddress.zipCode);
+            XCTAssertEqualObjects(person.address.state, referencedAddress.state);
+            XCTAssertEqualObjects(person.address.city, referencedAddress.city);
+            XCTAssertEqualObjects(person.address.other, referencedAddress.other);
+
+        }];
+    }
+}
+
+- (BOOL)archivePeopleInfo{
+    
+    NSArray<Person*>* personsArray = [self personsArray];
+    
+    NSString* filePath = [self filePath];
+    
+    return [NSKeyedArchiver archiveRootObject:personsArray
+                                       toFile:filePath];
+}
+
+-(NSArray<Person*>*)personsArray{
+    Person *tYamada = [[Person alloc] init];
+    tYamada.name = @"山田敏郎";
+    Address* yAddress = [[Address alloc] init];
+    yAddress.zipCode = @"104-0061";
+    yAddress.state = @"東京都";
+    yAddress.city = @"中央区";
+    yAddress.other = @"銀座１丁目";
+    tYamada.address = yAddress;
+    
+    Person* hYamada = [[Person alloc] init];
+    hYamada.name = @"山田ひかり";
+    hYamada.address = yAddress;
+    
+    Person *tanaka = [[Person alloc] init];
+    tanaka.name = @"田中翔太";
+    Address* tAddress = [[Address alloc] init];
+    tAddress.zipCode = @"145-0071";
+    tAddress.state = @"東京都";
+    tAddress.city = @"大田区";
+    tAddress.other = @"田園調布１丁目";
+    tanaka.address = tAddress;
+    
+    return @[tYamada,hYamada,tanaka];
+}
+
+-(NSString*)filePath{
+    NSString* documentsDirectoryPathString = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
+    return [documentsDirectoryPathString stringByAppendingPathComponent:@"data.dat"];
 }
 
 @end
